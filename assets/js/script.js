c9a2e2cc4c1b7eb06edd27fb2cc3660a23ddd6c2
allibrdoine <allibrodine81@gmail.com>
2022-06-26 12:06:37 -0500

    UI enhancements and syntax fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -47,23 +46,6 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
-
-// modal was triggered
-$("#task-form-modal").on("show.bs.modal", function() {
-  // clear values
-  $("#modalTaskDescription, #modalDueDate").val("");
-});
-
-// modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function() {
-  // highlight textarea
-  $("#modalTaskDescription").trigger("focus");
-});
-
-//add datepicker to modal
-$("#modalDueDate").datepicker({
-  minDate: 1
-});
 
 var auditTask = function(taskEl) {
   //get date from task element
@@ -84,45 +66,24 @@ var auditTask = function(taskEl) {
   }
 };
 
-// save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
-  // get form values
-  var taskText = $("#modalTaskDescription").val();
-  var taskDate = $("#modalDueDate").val();
-
-  if (taskText && taskDate) {
-    createTask(taskText, taskDate, "toDo");
-
-    // close modal
-    $("#task-form-modal").modal("hide");
-
-    // save in tasks array
-    tasks.toDo.push({
-      text: taskText,
-      date: taskDate
-    });
-
-    saveTasks();
-  }
-});
-
-
 $(".card .list-group").sortable({
   connectWith: $(".card .list-group"),
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function(event, ui) {
-    console.log(ui);
+  activate: function(event, ui) { 
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag"); 
   },
   deactivate: function(event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log(event);
+    $(event.target).removeClass("dropover-active");
   },
   update: function() {
     //array to store the task data in
@@ -152,31 +113,74 @@ $(".card .list-group").sortable({
     //update array on tasks object and save
     tasks[arrName] = tempArr;
     saveTasks();
-  }
+  },
+  //stop: function(event) {
+    //$(this).removeClass("dropover");
+  //}
 });
 
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
-  drop: function(event, ui) {
+  drop: function(event, ui) { 
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active"); 
   },
   over: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active"); 
   },
   out: function(event, ui) {
-    console.log(ui);
+    $("bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
+//add datepicker to modal
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+// modal was triggered
+$("#task-form-modal").on("show.bs.modal", function() {
+  // clear values
+  $("#modalTaskDescription, #modalDueDate").val("");
+});
+
+// modal is fully visible
+$("#task-form-modal").on("shown.bs.modal", function() {
+  // highlight textarea
+  $("#modalTaskDescription").trigger("focus");
+});
+
+
+// save button in modal was clicked
+$("#task-form-modal .btn-save").click(function() {
+  // get form values
+  var taskText = $("#modalTaskDescription").val();
+  var taskDate = $("#modalDueDate").val();
+
+  if (taskText && taskDate) {
+    createTask(taskText, taskDate, "toDo");
+
+    // close modal
+    $("#task-form-modal").modal("hide");
+
+    // save in tasks array
+    tasks.toDo.push({
+      text: taskText,
+      date: taskDate
+    });
+
+    saveTasks();
+  }
+});
 
 $(".list-group").on("click", "p", function() {
   var text = $(this).text().trim();
-  var textInput = $("<textarea>")
-    .addClass("form-control")
-    .val(text);
+  var textInput = $("<textarea>").addClass("form-control").val(text);
   $(this).replaceWith(textInput);
-    textInput.trigger("focus");
+
+  textInput.trigger("focus");
 });
 
 $(".list-group").on("blur", "textarea", function() {
@@ -253,12 +257,15 @@ $("#remove-tasks").on("click", function() {
     tasks[key].length = 0;
     $("#list-" + key).empty();
   }
+  console.log(tasks);
   saveTasks();
 });
-
-
 
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
